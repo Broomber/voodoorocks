3447b752d72bd595df13494855c6ef170b71aef0
Юрий <bondarenko.yuriy.1992@gmail.com>
2024-03-22 09:19:09 +0600

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
-  //   },
-  // }
+  base: "/voodoorocks",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
